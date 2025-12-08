dc6037b4caea87e9572c30405f81bac060d30bba
LucasLeitz <lucasleitz@outlook.com>
2025-12-08 18:47:30 -0500

    Fixing Health Controller to be used in my website

--- backend/src/main/java/com/lucasleitz/medialibrary/controllers/HealthController.java
+++ backend/src/main/java/com/lucasleitz/medialibrary/controllers/HealthController.java
@@ -1,11 +1,13 @@
 package com.lucasleitz.medialibrary.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class HealthController {
 
     @GetMapping("/health")
